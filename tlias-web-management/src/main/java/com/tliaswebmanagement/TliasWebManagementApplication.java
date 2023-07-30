93f5816ae7f8ffd9f927455a9087bb3fab281108
QAQLC <873228767@qq.com>
2023-07-30 17:00:52 +0800

    project init

--- tlias-web-management/src/main/java/com/tliaswebmanagement/TliasWebManagementApplication.java
+++ tlias-web-management/src/main/java/com/tliaswebmanagement/TliasWebManagementApplication.java
@@ -1,12 +1,11 @@
-package com.tlias;
+package com.tliaswebmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 
-@ServletComponentScan
 @SpringBootApplication
 public class TliasWebManagementApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(TliasWebManagementApplication.class, args);
     }
