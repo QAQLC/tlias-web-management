ed75f86000de9a809058fe0a3a123180d4eca63d
QAQLC <873228767@qq.com>
2023-08-10 23:26:30 +0800

    update

---
+++ src/main/java/com/tliaswebmanagement/controller/BaseController.java
@@ -0,0 +1,9 @@
+package com.tliaswebmanagement.controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BaseController {
+
+}
--- src/main/java/com/tliaswebmanagement/controller/HelloWorld.java
+++ src/main/java/com/tliaswebmanagement/controller/HelloWorld.java
@@ -3,7 +3,7 @@ package com.tliaswebmanagement.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
 public class HelloWorld {
     @GetMapping("/hello")
     public String hello () {
