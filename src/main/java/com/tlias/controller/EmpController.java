547502d8bb5607d8a4acb4a2d0da2146647044e5
QAQLC <745573545@qq.com>
2023-09-17 11:32:46 +0800

    P74 Day05-08 请求响应-响应-案例

--- src/main/java/com/tlias/controller/EmpController.java
+++ src/main/java/com/tlias/controller/EmpController.java
@@ -3,10 +3,9 @@ package com.tlias.controller;
 import com.tlias.pojo.Emp;
 import com.tlias.pojo.Result;
 import com.tlias.utils.XmlParseUtils;
+import java.util.List;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 /**
  * 解析xml
@@ -14,14 +13,14 @@ import java.util.List;
  */
 @RestController
 public class EmpController {
-    @RequestMapping("/listEmp")
-    public Result listEmp () {
-        String file = this.getClass().getClassLoader().getResource("emp.xml").getFile();
-        // 1.获取类路径下的emp.xml
-        List<Emp> empList = XmlParseUtils.parse(file, Emp.class);
 
-        empList.stream().forEach( emp  -> {
-            String  gender = emp.getGender();
+    @RequestMapping("/listEmp")
+    public Result listEmp() {
+        String file = this.getClass().getClassLoader().getResource("emp.xml").getFile();
+        // 获取数据持久层的数据
+        List<Emp> empList = XmlParseUtils.parse(file, Emp.class);
+        empList.forEach(emp -> {
+            String gender = emp.getGender();
             if ("1".equals(gender)) {
                 emp.setGender("男");
             } else {
@@ -29,11 +28,11 @@ public class EmpController {
             }
             String job = emp.getJob();
             if ("1".equals(job)) {
-                emp.setJob(("讲师"));
+                emp.setJob("讲师");
             } else if ("2".equals(job)) {
                 emp.setJob("班主任");
             } else {
-                emp.setJob("就业指导");
+                emp.setJob("就业老师");
             }
         });
         return Result.SUCCESS(empList);
